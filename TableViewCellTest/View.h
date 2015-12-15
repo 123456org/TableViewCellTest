ae23ea78616db5a4218e2023f7fb35b9ac0775a6
sylvanas0907 <929263706@qq.com>
2015-12-15 11:37:12 +0800

    test

---
+++ TableViewCellTest/View.h
@@ -0,0 +1,13 @@
+//
+//  View.h
+//  TableViewCellTest
+//
+//  Created by sylvan on 15/12/15.
+//  Copyright (c) 2015年 sylvan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface View : UIView
+
+@end
---
+++ TableViewCellTest/View.m
@@ -0,0 +1,21 @@
+//
+//  View.m
+//  TableViewCellTest
+//
+//  Created by sylvan on 15/12/15.
+//  Copyright (c) 2015年 sylvan. All rights reserved.
+//
+
+#import "View.h"
+
+@implementation View
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
